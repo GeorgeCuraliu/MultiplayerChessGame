eed5dca568f62471c99ffe6007419438c0bb449d
GeorgeCuraliu <georgecuraliu@gmail.com>
2023-12-23 16:26:52 +0200

    update for auth page

--- client/src/Auth/AuthPage.js
+++ client/src/Auth/AuthPage.js
@@ -8,11 +8,24 @@ const AuthPage = () => {
 
     const [authType, setAuthType] = useState(true);//will decide which of LoginContainer or CreateAccountContainer is displayed
 
+    const changeAuthMethod = () => {
+        setAuthType(val => !val);
+        console.log("chnaged auth type");
+    }
+
+    const login = (username, password) => {
+        console.log(`login with ${username} ${password}`)
+    }
+
     return (
         <div className={styles.pageContainer}>
             <img src={AuthIcon} className={styles.authIcon} />
             <div className={styles.inputContainer}>
-                {authType ? <LoginContainer /> : undefined}
+                {authType ? 
+                    <LoginContainer 
+                    changeAuthMethod={changeAuthMethod} 
+                    login={login}/> : 
+                    undefined}
             </div> 
         </div>
     )
--- client/src/Auth/LoginContainer.js
+++ client/src/Auth/LoginContainer.js
@@ -3,13 +3,26 @@ import style from "./auth.module.css";
 
 const LoginContainer = (props) => {
     
+    const handleLogin = (e) => {
+        e.preventDefault();
+        props.login("dwa", "wda");
+    }
+
     return(
-        <React.Fragment>
-            <p>LOGIN</p>
-            <input />
-            <input />
-            <p>CREATE AN ACCOUNT</p>
-        </React.Fragment>
+        <form onSubmit={e => handleLogin(e)} className={style.form}>
+            <input 
+                className={style.input} 
+                placeholder="username"/>
+            <input 
+                className={`${style.input} ${style.password0}`} 
+                placeholder="password"/>
+            <button 
+                type="confirm" 
+                className={style.authButton}>CONFIRM</button>
+            <button 
+                className={`${style.changeAuth} ${style.authButton}`} 
+                onClick={props.changeAuthMethod}>CREATE AN ACCOUNT</button>
+        </form>
     )
 }
 
